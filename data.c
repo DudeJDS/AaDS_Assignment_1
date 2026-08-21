c280e34fb4b2ab9e1bc206481af200a0e3314058
Jude Scott <judescott1034@gmail.com>
2026-08-21 16:19:44 +1000

    Submission V2

--- data.c
+++ data.c
@@ -14,54 +14,67 @@ wildlife_t *create_wildlife(char *fields[]) {
 
     int field_num = 0;
     data->key = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->key);
     strcpy(data->key, fields[field_num]);
     field_num++;
 
     data->taxa = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->taxa);
     strcpy(data->taxa, fields[field_num]);
     field_num++;
 
     data->kingdom = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->kingdom);
     strcpy(data->kingdom, fields[field_num]);
     field_num++;
 
     data->phylum = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->phylum);
     strcpy(data->phylum, fields[field_num]);
     field_num++;
 
     data->class_name = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->class_name);
     strcpy(data->class_name, fields[field_num]);
     field_num++;
 
     data->order = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->order);
     strcpy(data->order, fields[field_num]);
     field_num++;
 
     data->family = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->family);
     strcpy(data->family, fields[field_num]);
     field_num++;
 
     data->genus = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->genus);
     strcpy(data->genus, fields[field_num]);
     field_num++;
 
     data->species = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->species);
     strcpy(data->species, fields[field_num]);
     field_num++;
 
     data->common_name = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->common_name);
     strcpy(data->common_name, fields[field_num]);
     field_num++;
 
     data->identification = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->identification);
     strcpy(data->identification, fields[field_num]);
     field_num++;
 
     data->data_resource = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->data_resource);
     strcpy(data->data_resource, fields[field_num]);
     field_num++;
 
     data->sighting_data = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->sighting_data);
     strcpy(data->sighting_data, fields[field_num]);
     field_num++;
 
@@ -72,6 +85,7 @@ wildlife_t *create_wildlife(char *fields[]) {
     field_num++;
 
     data->easy_adress = malloc(strlen(fields[field_num]) + 1); // for '\0'
+    assert(data->easy_adress);
     strcpy(data->easy_adress, fields[field_num]);
 
     return data;
